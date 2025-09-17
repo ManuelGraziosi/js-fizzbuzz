2d616b9f8d68704e87d92726e0b47d34232b96a0
ManuelGraziosi <manuel.graziosi@gmail.com>
2025-09-17 20:38:59 +0200

    style: donato advice added

--- js/script.js
+++ js/script.js
@@ -25,17 +25,16 @@
  * stampare result
  */
 
-let result = null;
 
 for (let i = 1; i <= 100; i++) {
+    let result = `${i}`;
+
     if (i % 3 === 0 && i % 5 === 0) {
         result = "FizzBuzz";
     } else if (i % 3 === 0) {
         result = "Fizz";
     } else if (i % 5 === 0) {
         result = "Buzz";
-    } else {
-        result = i;
     }
 
     console.log(result);
